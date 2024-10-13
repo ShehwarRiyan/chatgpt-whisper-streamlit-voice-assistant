9766a84797fb4c2bad50b8124eed14099adf85ab
ShehwarRiyan <158547498+ShehwarRiyan@users.noreply.github.com>
2024-10-13 18:03:02 +0300

    Update myapikeys.py

--- myapikeys.py
+++ myapikeys.py
@@ -7,4 +7,4 @@ Original file is located at
     https://colab.research.google.com/drive/1gtoyFG6AkQiNlB46gf83vfahftOTKBb2
 """
 
-OPENAI_KEY = "sk-XHbxcz6JeONf5_xUjUhpkuHoeAPSyenYNaMBNyZ3z-T3BlbkFJmYhTtY8fSEIOItCmN5m-ERvfqnMIfo9fJsis5uYSwA"
+#OPENAI_KEY = "sk-XHbxcz6JeONf5_xUjUhpkuHoeAPSyenYNaMBNyZ3z-T3BlbkFJmYhTtY8fSEIOItCmN5m-ERvfqnMIfo9fJsis5uYSwA"
